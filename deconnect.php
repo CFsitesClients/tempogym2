2ab4f44cbf3e26d5d4f69b5c2eecf10a600ff3b6
Stagiaire <Stagiaire@192.168.178.253>
2015-09-14 09:52:37 +0200

    deconnect cache

--- deconnect.php
+++ deconnect.php
@@ -3,10 +3,10 @@ session_start();
 // suppresion des variables de session
 $_SESSION = array();
 // destruction du cookie si ils sont activés
-if(ini_get("session.use_cookies")){
+if (ini_get("session.use_cookies")) {
     $parametre = session_get_cookie_params();
     // on écrase le cookie de session avec un cookie périmé
-    setcookie(session_name(),'',time()-42000,$parametre['path'],$parametre['domain'],$parametre['secure'],$parametre['httponly']);
+    setcookie(session_name(), '', time() - 42000, $parametre['path'], $parametre['domain'], $parametre['secure'], $parametre['httponly']);
 }
 // destruction du fichier de session côté serveur
 session_destroy();
